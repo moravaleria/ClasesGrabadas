c22360a8ad320d5789d6552b1cea887896f09880
Valeria Mora Cardenas <vamorac@unal.edu.co>
2026-04-05 22:52:12 -0500

    5. Relación entre modelos (De 1 a muchos)parte 2

--- ClasesGrabadas/app/Models/Comment.php
+++ ClasesGrabadas/app/Models/Comment.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post(){
+        $this->belongsTo(Post::class);
+    }
 }
--- ClasesGrabadas/app/Models/Post.php
+++ ClasesGrabadas/app/Models/Post.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\user;
+use App\Models\Comment;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
+
+    public function comments(){
+        $this->hasMany(Comment::class);
+    }
+
+
 }
