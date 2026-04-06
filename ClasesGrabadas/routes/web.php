d7d1917c895a80675352aea5773baca78076948b
Valeria Mora Cardenas <vamorac@unal.edu.co>
2026-04-06 00:29:47 -0500

    6.CRUD minuto 21:00 -Controller,view,routes

--- ClasesGrabadas/routes/web.php
+++ ClasesGrabadas/routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
